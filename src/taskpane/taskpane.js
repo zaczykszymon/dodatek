903c86e64b4b1ea774e69158e10cf5cd9223aea3
zaczykszymon <szymon.zaczyk@pergam.in>
2024-09-25 14:40:01 +0200

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global document, Office, Word */
-
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
@@ -15,16 +8,8 @@ Office.onReady((info) => {
 
 export async function run() {
   return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
-
-    // insert a paragraph at the end of the document.
     const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
     paragraph.font.color = "blue";
-
     await context.sync();
   });
 }

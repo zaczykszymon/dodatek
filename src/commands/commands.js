e7ffe30187021895c6b01fab9834e961e6ed74c9
zaczykszymon <szymon.zaczyk@pergam.in>
2024-09-25 14:50:41 +0200

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,13 +1,13 @@
-Office.onReady(() => {
-});
-function action(event) {
-  const message = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
-  event.completed();
+function openSignTaskpane() {
+    Office.context.ui.displayDialogAsync(
+        'https://raw.githubusercontent.com/zaczykszymon/dodatek/main/src/taskpane/taskpane.html',
+        { height: 50, width: 30 },
+        function (asyncResult) {
+            if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                console.error("Failed to open the taskpane:", asyncResult.error.message);
+            } else {
+                console.log("Taskpane opened successfully.");
+            }
+        }
+    );
 }
-Office.actions.associate("action", action);

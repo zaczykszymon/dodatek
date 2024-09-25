5f74a3cc9a808c50e0ebdef879e96572f8988525
zaczykszymon <szymon.zaczyk@pergam.in>
2024-09-25 14:52:53 +0200

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,15 +1,5 @@
-Office.onReady((info) => {
-  if (info.host === Office.HostType.Word) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
+Office.onReady(function(info) {
+    if (info.host === Office.HostType.Word) {
+        console.log("Pergamin taskpane loaded.");
+    }
 });
-
-export async function run() {
-  return Word.run(async (context) => {
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-    paragraph.font.color = "blue";
-    await context.sync();
-  });
-}
